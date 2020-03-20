48cdb8bab86484e1325d736b13b0a1d584234890
karanr1990 <33133108+karanr1990@users.noreply.github.com>
2020-03-21 01:25:48 +0530

    Create create-single-record.go

---
+++ src/create-single-record.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"log"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "root"
+	dbname   = "golangdb"
+)
+
+type UserModel struct{
+	Id int `gorm:"primary_key";"AUTO_INCREMENT"`
+	Name string `gorm:"size:255"`
+	Address string `gorm:"type:varchar(100)”`
+}
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := gorm.Open("postgres", psqlInfo)
+	defer db.Close()
+	if err != nil {
+		log.Println("Connection Failed to Open")
+	}
+
+	log.Println("Connection Established")
+
+	//Drops table if already exists
+	db.Debug().DropTableIfExists(&UserModel{})
+
+	//Auto create table based on Model
+	db.Debug().AutoMigrate(&UserModel{})
+
+	user := &UserModel{Name:"karan",Address:"Rajasthan"}
+
+	db.Create(user)
+
+	log.Println("Record Inserted")
+
+}
