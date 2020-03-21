4f3cf59db57f4fc84e6aeb27a6e020492c696ee7
karanr1990 <33133108+karanr1990@users.noreply.github.com>
2020-03-21 12:02:07 +0530

    Create delete-record.go

---
+++ src/delete-record.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"log"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "root"
+	dbname   = "golangdb"
+)
+
+type UserModel struct{
+	Id int `gorm:"primary_key";"AUTO_INCREMENT"`
+	Name string `gorm:"size:255"`
+	Address string `gorm:"type:varchar(100)”`
+}
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := gorm.Open("postgres", psqlInfo)
+	defer db.Close()
+	if err != nil {
+		log.Println("Connection Failed to Open")
+	}
+
+	log.Println("Connection Established")
+
+	//Drops table if already exists
+	//db.Debug().DropTableIfExists(&UserModel{})
+
+	//Auto create table based on Model
+	db.Debug().AutoMigrate(&UserModel{})
+
+	// select records to delete it
+	db.Table("user_models").Where("address = ?", "Houston").Delete(&UserModel{})
+
+	log.Println("batch record Updated")
+
+}
